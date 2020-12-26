d0c61161b359ecca0d5bc7e5c4324633f1541266
adominus <1979748+adominus@users.noreply.github.com>
2020-12-26 15:57:12 +0000

    Basic Version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
-import crackerImg from './assets/cracker.jpg';
+import nibblesImg from './assets/nibbles.jpeg';
+import bangImg from './assets/bang.png';
+import crackerLhsImg from './assets/cracker-lhs.png';
+import crackerRhsImg from './assets/cracker-rhs.png';
 import powMp3 from './assets/e.mp3';
 import powM4a from './assets/e.m4a';
 
@@ -13,8 +15,10 @@ class MyGame extends Phaser.Scene
 	
     preload ()
     {
-        this.load.image('logo', logoImg);
-        this.load.image('cracker', crackerImg);
+        this.load.image('nibbles', nibblesImg);
+        this.load.image('bang', bangImg);
+        this.load.image('cracker-lhs', crackerLhsImg);
+        this.load.image('cracker-rhs', crackerRhsImg);
         //this.load.audio('pow', ['https://raw.githubusercontent.com/photonstorm/phaser-examples/master/examples/assets/audio/SoundEffects/explosion.mp3']);
         //this.load.audio('pow', ['https://raw.githubusercontent.com/photonstorm/phaser-examples/master/examples/assets/audio/SoundEffects/explode1.wav']);
         this.load.audio('pow', [powMp3, powM4a]);
@@ -22,21 +26,24 @@ class MyGame extends Phaser.Scene
       
     create ()
     {
-	/*const gameHeight = game.config.height;
+	const gameHeight = game.config.height;
 	const gameWidth = game.config.width;
 	const centreX = gameWidth / 2;
 	const centreY = gameHeight / 2;
+	const padding = 80;
+	let bg = this.add.image(centreX, centreY, 'nibbles');
+	bg.setDisplaySize(gameWidth, gameHeight);
 
-        const cracker1 = this.add.image(400, 150, 'cracker');
-	const crackerHeight = cracker1.height / 2;
-	const crackerWidth = cracker1.width / 2;
-	cracker1.x = centreX - crackerWidth;
-	cracker1.y = centreY - crackerHeight;
+        const crackerLhs = this.add.image(400, 150, 'cracker-lhs');
+	const crackerHeight = crackerLhs.height / 2;
+	const crackerWidth = crackerLhs.width / 2;
+	crackerLhs.x = centreX - crackerWidth + padding;
+	crackerLhs.y = centreY + crackerHeight - padding;
 
-        const cracker2 = this.add.image(centreX + crackerWidth, centreY + crackerHeight, 'cracker');
+        const crackerRhs = this.add.image(centreX + crackerWidth - padding, centreY - crackerHeight + padding, 'cracker-rhs');
 
 	const explode1 = this.tweens.add({
-			targets: cracker1,
+			targets: crackerLhs,
 			y: 100,
 			x: 100,
 			duration: 500,
@@ -45,7 +52,7 @@ class MyGame extends Phaser.Scene
 		});
 
 	const explode2 = this.tweens.add({
-			targets: cracker2,
+			targets: crackerRhs,
 			y: game.config.height - 100,
 			x: game.config.width - 100,
 			duration: 500,
@@ -54,47 +61,46 @@ class MyGame extends Phaser.Scene
 			onComplete: this.crackerPopped,
 			onCompleteScope: this
 		});
-		//*/
 
 	const pow = this.sound.add('pow');
 
 	this.state = 1;
-	let scene = this;
 
 	this.input.on('pointerup', function () {
-		//if (this.state == 1) {
-			//if (explode1.isPaused) {
-				//explode1.play();
-				//explode2.play();
-				pow.play();
-			//}
-		//} else if (this.state == 2) {
-			//this.add.text(100, centreY - 100, 'lorem ipsum dolar sit amet lorem ipsum dolar sit amet lorem ipsum dolar sit amet lorem ipsum dolar sit amet', 
-				//{ font: '50px Arial', fill: 'white', wordWrap: { width: gameWidth - 200 }, align: 'center' });
-//
-			//this.state = 3
-		//} else if (this.state == 3) {
-			//console.log('restart');
-			//this.scene.restart();
-		//}
+		if (this.state == 1) {
+		    if (explode1.isPaused) {
+			    this.crackerPopped();
+			    explode1.play();
+			    explode2.play();
+			    pow.play();
+		    }
+		} else if (this.state == 2) {
+			let text = this.add.text(100, centreY - 100, 'How do weavers communicate during lockdown?\n\nVia Loom', 
+				{ font: '50px Arial', fill: 'white', wordWrap: { width: gameWidth - 200 }, align: 'center' , stroke: 'black', strokeThickness: 8});
+
+			this.state = 3
+		} else if (this.state == 3) {
+			console.log('restart');
+			this.scene.restart();
+		}
 	}, this);
 
     }
 
     update () { }
 
-	crackerPopped(tween, targets) {
-		const gameHeight = game.config.height;
-		const gameWidth = game.config.width;
-		const centreX = gameWidth / 2;
-		const centreY = gameHeight / 2;
+    crackerPopped(tween, targets) {
+	    const gameHeight = game.config.height;
+	    const gameWidth = game.config.width;
+	    const centreX = gameWidth / 2;
+	    const centreY = gameHeight / 2;
 
-        const cracker2 = this.add.image(
-			centreX, 
-			centreY, 'logo');
-	
-		this.state = 2;
-	}
+	    const bang = this.add.image(
+			    centreX, 
+			    centreY, 'bang');
+    
+	    this.state = 2;
+    }
 }
 
 const config = {
