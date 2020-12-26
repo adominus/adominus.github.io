5a2103c821cf1630d68f0e8a78cb213d7788b55c
adominus <1979748+adominus@users.noreply.github.com>
2020-12-26 14:36:35 +0000

    Multiple audio files

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ class MyGame extends Phaser.Scene
         this.load.image('cracker', crackerImg);
         //this.load.audio('pow', ['https://raw.githubusercontent.com/photonstorm/phaser-examples/master/examples/assets/audio/SoundEffects/explosion.mp3']);
         //this.load.audio('pow', ['https://raw.githubusercontent.com/photonstorm/phaser-examples/master/examples/assets/audio/SoundEffects/explode1.wav']);
-        this.load.audio('pow', [powMp3]);
+        this.load.audio('pow', [powMp3, 'https://raw.githubusercontent.com/photonstorm/phaser-examples/master/examples/assets/audio/SoundEffects/explode1.wav', 'https://raw.githubusercontent.com/photonstorm/phaser-examples/master/examples/assets/audio/SoundEffects/fx_mixdown.ogg']);
     }
       
     create ()
