ddd12a351ceffe7d86a5926e42a9a213bc2812f5
adominus <1979748+adominus@users.noreply.github.com>
2020-12-26 14:24:18 +0000

    Add sound

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
 import logoImg from './assets/logo.png';
 import crackerImg from './assets/cracker.jpg';
+import powMp3 from './assets/e.mp3';
 
 class MyGame extends Phaser.Scene
 {
@@ -13,82 +14,72 @@ class MyGame extends Phaser.Scene
     {
         this.load.image('logo', logoImg);
         this.load.image('cracker', crackerImg);
+        //this.load.audio('pow', ['https://raw.githubusercontent.com/photonstorm/phaser-examples/master/examples/assets/audio/SoundEffects/explosion.mp3']);
+        //this.load.audio('pow', ['https://raw.githubusercontent.com/photonstorm/phaser-examples/master/examples/assets/audio/SoundEffects/explode1.wav']);
+        this.load.audio('pow', [powMp3]);
     }
       
     create ()
     {
-		console.log(game);
-
-		const gameHeight = game.config.height;
-		const gameWidth = game.config.width;
-		const centreX = gameWidth / 2;
-		const centreY = gameHeight / 2;
+	const gameHeight = game.config.height;
+	const gameWidth = game.config.width;
+	const centreX = gameWidth / 2;
+	const centreY = gameHeight / 2;
 
         const cracker1 = this.add.image(400, 150, 'cracker');
-		const crackerHeight = cracker1.height / 2;
-		const crackerWidth = cracker1.width / 2;
-		cracker1.x = centreX - crackerWidth;
-		cracker1.y = centreY - crackerHeight;
+	const crackerHeight = cracker1.height / 2;
+	const crackerWidth = cracker1.width / 2;
+	cracker1.x = centreX - crackerWidth;
+	cracker1.y = centreY - crackerHeight;
 
         const cracker2 = this.add.image(centreX + crackerWidth, centreY + crackerHeight, 'cracker');
 
-		console.log(cracker1);
+	const explode1 = this.tweens.add({
+			targets: cracker1,
+			y: 100,
+			x: 100,
+			duration: 500,
+			ease: "Power2",
+			paused: true
+		});
 
-		const explode1 = this.tweens.add({
-				targets: cracker1,
-				y: 100,
-				x: 100,
-				duration: 500,
-				ease: "Power2",
-				paused: true
-			});
-      
-		const explode2 = this.tweens.add({
-				targets: cracker2,
-				y: game.config.height - 100,
-				x: game.config.width - 100,
-				duration: 500,
-				ease: "Power2",
-				paused: true,
-				onComplete: this.crackerPopped,
-				onCompleteScope: this
-			});
-      
-		let state = '1';
-		let scene = this;
+	const explode2 = this.tweens.add({
+			targets: cracker2,
+			y: game.config.height - 100,
+			x: game.config.width - 100,
+			duration: 500,
+			ease: "Power2",
+			paused: true,
+			onComplete: this.crackerPopped,
+			onCompleteScope: this
+		});
 
-		this.input.on('pointerup', function () {
-			if (state == '1') {
-				console.log('playing');
+	const pow = this.sound.add('pow');
+
+	this.state = 1;
+	let scene = this;
+
+	this.input.on('pointerup', function () {
+		if (this.state == 1) {
+			if (explode1.isPaused) {
 				explode1.play();
 				explode2.play();
-
-				state = '2';
-			} else if (state == '2') {
-				this.add.text(100, centreY - 100, 'lorem ipsum dolar sit amet lorem ipsum dolar sit amet lorem ipsum dolar sit amet lorem ipsum dolar sit amet', 
-					{ font: '50px Arial', fill: 'white', wordWrap: { width: gameWidth - 200 }, align: 'center' });
-
-				state = '3'
-			} else if (state == '3') {
-				console.log('restart');
-				this.scene.restart();
+				pow.play();
 			}
-		}, this);
+		} else if (this.state == 2) {
+			this.add.text(100, centreY - 100, 'lorem ipsum dolar sit amet lorem ipsum dolar sit amet lorem ipsum dolar sit amet lorem ipsum dolar sit amet', 
+				{ font: '50px Arial', fill: 'white', wordWrap: { width: gameWidth - 200 }, align: 'center' });
+
+			this.state = 3
+		} else if (this.state == 3) {
+			console.log('restart');
+			this.scene.restart();
+		}
+	}, this);
 
     }
 
-    update () {
-	}
-
-	pointerUp() {
-		console.log(this);
-
-	}
-
-	popCracker() {
-		this.explode1.play();
-		this.explode2.play();
-	}
+    update () { }
 
 	crackerPopped(tween, targets) {
 		const gameHeight = game.config.height;
@@ -99,6 +90,8 @@ class MyGame extends Phaser.Scene
         const cracker2 = this.add.image(
 			centreX, 
 			centreY, 'logo');
+	
+		this.state = 2;
 	}
 }
 
